4e9d1eed1532a80c516d9de63c021cbaf183746e
KIM\Kim <kimtischler1132@gmail.com>
2017-11-24 11:48:22 +0100

    Kim
    Turret fixed and can shooting

---
+++ Tank/Assets/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float damage;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.tag);
+
+
+        // Find the TankHealth script associated with the rigidbody.
+        Complete.TankHealth targetHealth = other.GetComponent<Complete.TankHealth>();
+        // If there is no TankHealth script attached to the gameobject, go on to the next collider.
+        if (!targetHealth)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        print(damage);
+        // Deal this damage to the tank.
+        targetHealth.TakeDamage(damage);
+        Destroy(gameObject);
+    }
+}
--- Tank/Assets/_Completed-Assets/Scripts/Turrets/Turret.cs
+++ Tank/Assets/_Completed-Assets/Scripts/Turrets/Turret.cs
@@ -12,9 +12,18 @@ public class Turret : MonoBehaviour {
     public Transform partToRotate;
     public float turnSpeed = 7f;
 
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float fireCd;
+    public float bulletSpeed;
+
+    private float fireCdTmp;
     private GameObject[] _enemy;
 
-    
+    void Start()
+    {
+        fireCdTmp = fireCd;
+    }
 
     void UpdateTarget () {
        
@@ -25,7 +34,7 @@ public class Turret : MonoBehaviour {
             float distanceToEnemy = Vector3.Distance(transform.position, e.transform.position);
 
             Complete.TankShooting t = e.gameObject.GetComponent<Complete.TankShooting>();
-            print(t.m_PlayerNumber);
+          
                 if (distanceToEnemy < shortestDistance  && playerNumber != e.GetComponent<Complete.TankShooting>().m_PlayerNumber)
                 {
                     shortestDistance = distanceToEnemy;
@@ -48,20 +57,44 @@ public class Turret : MonoBehaviour {
         _enemy = GameObject.FindGameObjectsWithTag(enemyTag);
         UpdateTarget();
 
-        print(_enemy.Length);
+
 
         if (target == null)
+        {
             return;
+        }
 
         Vector3 dir = target.position - transform.position;
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed ).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        fireCdTmp -= Time.deltaTime;
+        if (target != null&& fireCdTmp <= 0)
+        {
+            Fire();
+            fireCdTmp = fireCd;
+        }
     }
 
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+    void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 5.0f);
     }
 }
