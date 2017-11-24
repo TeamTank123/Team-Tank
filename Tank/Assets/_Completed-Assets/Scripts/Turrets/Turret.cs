fe7f0cf62dd488bff43edda08ebdce672e4e6ba6
Q4drO <kimtischler1132@gmail.com>
2017-11-24 01:43:12 +0100

    Kim
    spawn area added , turret fixed

--- Tank/Assets/_Completed-Assets/Scripts/Turrets/Turret.cs
+++ Tank/Assets/_Completed-Assets/Scripts/Turrets/Turret.cs
@@ -57,7 +57,7 @@ public class Turret : MonoBehaviour {
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed ).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
-	}
+    }
 
     void OnDrawGizmosSelected()
     {
