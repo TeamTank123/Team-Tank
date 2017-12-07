5948727f66850d599305bc1bb3b0c18f23f1392a
KIM\Kim <kimtischler1132@gmail.com>
2017-12-07 19:43:01 +0100

    kim

--- Tank/Assets/Bullet.cs
+++ Tank/Assets/Bullet.cs
@@ -10,18 +10,20 @@ public class Bullet : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
 
-
-
-        // Find the TankHealth script associated with the rigidbody.
-        Complete.TankHealth targetHealth = other.GetComponent<Complete.TankHealth>();
-        // If there is no TankHealth script attached to the gameobject, go on to the next collider.
-        if (!targetHealth)
+        if (other.tag.Equals("Canister") || other.tag.Equals("Shield")) { }
+        else
         {
+            // Find the TankHealth script associated with the rigidbody.
+            Complete.TankHealth targetHealth = other.GetComponent<Complete.TankHealth>();
+            // If there is no TankHealth script attached to the gameobject, go on to the next collider.
+            if (!targetHealth)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            // Deal this damage to the tank.
+            targetHealth.TakeDamage(damage);
             Destroy(gameObject);
-            return;
         }
-        // Deal this damage to the tank.
-        targetHealth.TakeDamage(damage);
-        Destroy(gameObject);
     }
 }
